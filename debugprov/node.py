342c1a44537f8f40f06761d351e67829845f67cd
Romer <romerlucas@gmail.com>
2020-11-26 23:20:38 -0300

    SIM

--- debugprov/node.py
+++ debugprov/node.py
@@ -70,7 +70,7 @@ class Node:
                           #'type_name':str(type(x.name)), 
                           'str_value':str(x.value), 
                           'type_value':str(type(x.value))} for x in self.params[::-1]][dicio['pilhas'].get(self.name)],
-                          '''
+            '''
             #'indext':float(self.params[0][0])
             #'indext':dicio['pilhas'].get(self.name),
             }
