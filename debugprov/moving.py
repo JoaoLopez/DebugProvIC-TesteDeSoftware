58ccb693da79b429a4d742536b086f832e870742
Romer <romerlucas@gmail.com>
2020-12-14 01:02:44 -0300

    SIM

--- debugprov/moving.py
+++ debugprov/moving.py
@@ -34,7 +34,7 @@ def MyMove():
         pass
     ambientepy=ambiente+".py"
     print("AMBIENTPY: ", ambientepy)
-    shutil.copyfile(f"{ambiente}.py", adress(pastah[2:]+ambientepy))
+    shutil.copyfile(f"{ambiente}.py.", adress(pastah[2:]+ambientepy))
     
     if '.noworkflow' in os.listdir():
         shutil.move(adress('.noworkflow'), adress(pastah))
