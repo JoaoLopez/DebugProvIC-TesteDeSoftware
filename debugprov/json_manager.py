b2a3611d851b17d3d54b3c0ff9c0dec4606b209e
Romer <romerlucas@gmail.com>
2021-02-03 14:59:22 -0300

    node_dict

--- debugprov/json_manager.py
+++ debugprov/json_manager.py
@@ -7,17 +7,17 @@ def create_json(new_data = {}):
     with open(name_json(), "w") as json_file:
         json.dump(new_data, json_file, indent=4)
 
-def add_node_to_json(nodo):
+def add_node_to_json(node):
     try:
         with open(name_json(), 'r') as json_file:
             my_json = json.load(json_file)
     except:
         my_json = dict()
     print("==dicio==")
-    nodo = nodo.__dict__
-    print(nodo)
+    node_dict = node.__dict__
+    print(node)
     pprint(my_json)
-    my_json[nodo['ev_id']] = nodo 
+    my_json[nodo['ev_id']] = node_dict 
     create_json(my_json)
 
 def name_json():
