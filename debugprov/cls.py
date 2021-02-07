bdf4b266303d11cad24f2d3afa0481f93d016d01
Romer <romerlucas@gmail.com>
2021-02-07 16:44:12 -0300

    implementa a higienizacao da pasta antes da execucao do noworkflow e cria um json vazio para ser utilizado

---
+++ debugprov/cls.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import os
+import sys
+import shutil
+import datetime
+def printf(string):
+    if not(sys.argv[1]):
+        print(string)
+# Get directory name
+
+def limpa():
+    if len(sys.argv)==1:
+        sys.argv.append(False)
+    print("arg=", sys.argv[1], type(sys.argv[1]))
+    mydir= '.noworkflow'
+    hoje = str(datetime.date.today())
+    ## Try to remove tree; if failed show an error using try...except on screen
+    '''try:
+        #shutil.rmtree(mydir)
+        print("Pasta .noworkflow apagada")
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))'''
+    try:
+        ehGv=lambda nome:nome[:4].isdigit()
+        gvs=list(filter(ehGv, os.listdir()))
+        adress=lambda n: os.getcwd()+'\\'+n
+        for item in gvs:
+            try:
+                dia=os.path.getmtime(item)
+                dia=datetime.datetime.fromtimestamp(dia).strftime('%Y-%m-%d')
+                os.makedirs(f'.\\logs\\{dia}')
+            except:
+                pass
+            novo=f'logs\\{dia}\\{item}'
+            shutil.move(adress(item), adress(novo))
+            printf(f'Transferido: {item}')
+    except:
+        printf("fail trivial")
+    try:
+        if '.noworkflow' in os.listdir():
+            ehGv=lambda nome:nome[:4]=='exec'
+            gvs=list(filter(ehGv, os.listdir()))
+            ag=datetime.datetime.now()
+            ag=ag.strftime('%Y-%m-%d(%H;%M)')
+            os.makedirs(f'.\\logs\\oldflows\\{ag}')
+            novo=f'.\\logs\\oldflows\\{ag}\\'
+            shutil.move(adress('.noworkflow'), adress(novo))
+            for item in gvs:
+                novo2=f'{novo}\\{{item}}'
+                shutil.move(adress(item), adress(novo))
+            printf("Cópia feita")
+        else:
+            printf("A pasta não existe")
+    except:
+        printf('fail .noworkflow')
+        
+if __name__=='__main__':
+    limpa()
--- debugprov/main.py
+++ debugprov/main.py
@@ -1,10 +1,14 @@
 from debugprov.console_interface import ConsoleInterface
 from debugprov.now_interface import NowInterface
 import traceback
+from debugprov.json_manager import create_json
+from debugprov.cls import limpa
 
 def main():
       try:
+            limpa()
             NowInterface().run_script()
+            create_json()
             ConsoleInterface().run()
       except:
             traceback.print_exc()      
