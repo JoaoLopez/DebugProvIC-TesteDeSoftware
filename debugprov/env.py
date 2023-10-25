890f86206dfaf930d44e750358566ac95ec48da4
JoaoLopez <joao.p.lopez01@gmail.com>
2023-10-25 08:08:25 -0300

    Renomeando arquivo clean.py para env.py

--- debugprov/env.py
+++ debugprov/env.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-def remove_gvs_and_pdfs():
+def remove_gvs_and_pdfs_files():
     k=0
     for file in os.listdir():
         if "gv" in file.split("."):
@@ -9,13 +9,14 @@ def remove_gvs_and_pdfs():
             k+=1
     print(f"Foram excluídos {k} arquivos")   
 
-def remove_noworkflow():
+def remove_noworkflow_folder():
     if os.path.exists('.noworkflow'):
         shutil.rmtree('.noworkflow')
         print(".noworkflow foi removido")
     else:
         print(".noworkflow não estava presente")
-def clean():
-    remove_noworkflow()
-    remove_gvs_and_pdfs()
+
+def prepare_env():
+    remove_noworkflow_folder()
+    remove_gvs_and_pdfs_files()
 
