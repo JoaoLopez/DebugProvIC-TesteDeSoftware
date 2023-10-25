be1269737f6cc2f0a3ede851f9b264a03aeaf3b3
JoaoLopez <joao.p.lopez01@gmail.com>
2023-10-25 10:32:54 -0300

    Refatorando arquivos unit_tests.py e console_interface.py

--- debugprov/cli/unit_tests.py
+++ debugprov/cli/unit_tests.py
@@ -1,30 +1,37 @@
 import sys, os
 import shutil
 
-def create_unit_tests(all_nodes):
-    FILENAME = f"test_{sys.argv[0]}"
-    MODULE = sys.argv[0][:-3]
-    print("FILENAME: ", FILENAME)
-    base = "\n\n"
-    try:
-        with open(FILENAME, 'r', encoding='utf-8') as file:
-            for row in file:
-                pass
-            nps = int(row.split(":")[1])+1
-    except:
-        with open(FILENAME, 'w', encoding='utf-8') as file:
+from typing import List
+from debugprov.entities.main_script import MainScript
+from debugprov.entities.node import Node
+
+def __get_number_of_next_round(file_path:str):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for row in file:
             pass
-        base+= f'import unittest\n'
-        base+= f'from {MODULE} import *\n\n'
-        base+= f'class Test_{MODULE.title()}(unittest.TestCase):\n\n'
-        nps = 0
+        next_round = int(row.split(":")[1])+1
+        return next_round
+    
+def create_unit_tests(main_script: MainScript, all_nodes: List[Node]):
+    file_path = f"test_{main_script.name}"
+    test_code = "\n\n"
+
+    if os.path.isfile(file_path):
+        next_round = __get_number_of_next_round(file_path)
+    else:
+        test_code+= f'import unittest\n'
+        test_code+= f'from {main_script.name[:-3]} import *\n'
+        for script_name in main_script.imported_scripts:
+            test_code+= f'from {script_name[:-3]} import *\n'
+        test_code+= f'\nclass Test_{main_script.name[:-3].title()}(unittest.TestCase):\n\n'
+        next_round = 0
         
     for nd in all_nodes:
-        base+=(nd.node_to_test(nps))
+        test_code+=(nd.get_test_code(main_script, next_round))
     
-    base+= f"{' '*4}# End of round: {nps}"
-    with open(FILENAME, 'a', encoding='utf-8') as file:
-        file.write(base)
+    test_code+= f"{' '*4}# End of round: {next_round}"
+    with open(file_path, 'a+', encoding='utf-8') as file:
+        file.write(test_code)
     
 def execute_coverage():
     '''
